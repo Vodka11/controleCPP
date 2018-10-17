e8b343760de3f176cb3a398468e3a2da0a816adf
Adrien <adrienheffer@yahoo.fr>
2018-10-17 11:07:33 +0200

    questions 1 à 5 sans le tri de la liste

--- src/reel.cpp
+++ src/reel.cpp
@@ -7,11 +7,17 @@ int main(){
 
 int i;
 int j;
+int k;
+int l;
+
+string menu;
 float enter;
+float del;
 
 vector <float> numbers(0);
 
 cout<<"Bienvenue dans le programme de stockage de réels"<<endl;
+cout<<"vous pourrez supprimer un ou plusieurs nombres après en avoir saisi dans la base"<<endl;
 cout<<"Merci d'entrer un réel, entrez 0 pour arrêter la saisie"<<endl;
 
 
@@ -29,8 +35,33 @@ cout<<"vous avez entré les nombres suivants : "<<endl;
 
 for(j=0; j<numbers.size(); j++){
 cout<<numbers[j]<<endl;
+
+
 }
 
+cout<<"appuyez sur la lettre a pour supprimer un nombre"<<endl;
+cin>>menu;
+
+if( menu == "a"){
+cout<<"choisissez un réel à bannir la première fois dans la liste"<<endl;
+cin>>del;
+for(k=0; k<numbers.size(); k++){
+if(numbers[k] == del){
+numbers[k] = 0;
+}
+}
+}
+
+ if( menu == "b"){
+cout<<"choisissez un réel à bannir de toute la liste"<<endl;
+cin>>del;
+for(l=0; l<numbers.size(); l++){
+if(numbers[l] == del){
+numbers[l] = 0;
+l++;
+}
+}
+ 
 }else{
 
 return 0;
@@ -38,6 +69,6 @@ return 0;
 
 }
 }
-
+}
 return 0;
 }
