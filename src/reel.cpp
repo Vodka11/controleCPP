427d1b52a061ff4117f49761f5de84d82b17a7b8
Adrien <adrienheffer@yahoo.fr>
2018-10-17 12:06:17 +0200

    ajout de commentaires mannuels

--- src/reel.cpp
+++ src/reel.cpp
@@ -5,6 +5,8 @@ using namespace std;
 
 int main(){
 
+// initialisation des variables 
+
 int i;
 int j;
 int k;
@@ -19,9 +21,14 @@ float tri = 0;
 
 vector <float> numbers(0);
 
+// affichage du menu 
+
 cout<<"Bienvenue dans le programme de stockage de réels"<<endl;
 cout<<"vous pourrez supprimer un ou plusieurs nombres après en avoir saisi dans la base"<<endl;
 cout<<"Merci d'entrer un réel, entrez 0 pour arrêter la saisie"<<endl;
+
+
+// suppression d'un nombre à enlever en premier dans la liste
 
 
 for(i=0; i< 100; i++){
@@ -33,14 +40,14 @@ if(enter != 0){
 numbers.push_back(enter);
 
 i++;
-
+// affichage des nombres saisis par l'utilisateur 
 cout<<"vous avez entré les nombres suivants : "<<endl;
 
 for(j=0; j<numbers.size(); j++){
 cout<<numbers[j]<<endl;
 }
 
-
+// tri des nombres + affichage
 cout<<"affichage après tri"<<endl;
 
 for(m=0; m<numbers.size(); m++){
@@ -56,10 +63,6 @@ n++;
 cout<<numbers[j]<<endl;
 }
 
-
-cout<<"appuyez sur la lettre a pour supprimer un nombre"<<endl;
-cin>>menu;
-
 if( menu == "a"){
 cout<<"choisissez un réel à bannir la première fois dans la liste"<<endl;
 cin>>del;
@@ -69,8 +72,11 @@ numbers[k] = 0;
 }
 }
 }
+// bannir un nombre de toute la liste
+cout<<"appuyez sur la lettre b pour supprimer un nombre de toute la liste"<<endl;
+cin>>menu;
 
- if( menu == "b"){
+if( menu == "b"){
 cout<<"choisissez un réel à bannir de toute la liste"<<endl;
 cin>>del;
 for(l=0; l<numbers.size(); l++){
@@ -79,14 +85,15 @@ numbers[l] = 0;
 l++;
 }
 }
- 
+
+// quitter si l'utilisateur entre 0
 }else{
 
 return 0;
 
-
 }
 }
 }
+//fin du programme
 return 0;
 }
